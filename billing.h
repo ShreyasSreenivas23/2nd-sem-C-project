7d5049749fea563b424237f702a04aff21d873c7
ShreyasSreenivas23 <165572768+ShreyasSreenivas23@users.noreply.github.com>
2024-04-24 21:48:39 +0530

    Delete billing.h

--- billing.h
+++
@@ -1,9 +0,0 @@
-#ifndef BILLING_H
-#define BILLING_H
-
-#include "inputing.h"
-#include "choose.h"
-
-void printBill(struct Custdetail customer, struct Item items[], int quantities[], int numItems, float total);
-
-#endif
